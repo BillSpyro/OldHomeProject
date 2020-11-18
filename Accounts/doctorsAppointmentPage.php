064f357a84b0b6c86ca378e0f240ee0970922519
Melaku <alebachewtegegne2017@yahoo.com>
2020-11-18 08:46:07 -0500

    doctors appointment page works properly add some css add anchor link for this page so the supervisors and admins can see it

--- Accounts/doctorsAppointmentPage.php
+++ Accounts/doctorsAppointmentPage.php
@@ -8,7 +8,7 @@
   <link href="../script/style.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body class="addditionalInfo">
+<body class="doctorsAppointment">
   <!-- basic patient registration form page -->
 <?php
 include "../Includes/header.php";
@@ -16,35 +16,31 @@ include "doctors_appointment.php";
 ?>
 <div class="Dr-appointment">
 <h1>Doctor's Appointment</h1>
-<form action="doctorsAppointmentPage.php" method="post">
-    <p>
-        <label for="id">Patient ID:</label>
-        <input type="text" name="id" id="id" required>
-    </p>
-     <p>
-        <label for="roster_date">Date:</label>
-        <input type="date" name="roster_date" id="date" required>
-    </p>
-    <input class="save" name="drAppointment_search" type="submit" value="Search">
+<form class="Dr_appointment" action="doctorsAppointmentPage.php" method="post">
+        <input type="text" name="patient_id" id="patient_id" placeholder="patient_id" required>
+        <input type="date" name="roster_date" id="roster_date"  >
+        
+        <input class="save" name="drAppointment_search" type="submit" value="Search">
 </form>
-
 <?php if (isset($_POST['drAppointment_search'])): ?>
 <form action="doctorsAppointmentPage.php" method="post">
+    <label for="patient_id">Patient ID:</label>
+    <input type="number" name="patient_id" value="<?php echo $patient_id; ?>" readonly >
+    
+    <label for="name">Patient Name:</label>
 
-    <p>
-    <label for="first_name">Patient Name:</label>
+    <input type="text" name="name" value="<?php echo $first_name . " ". $last_name; ?>" readonly >
 
-    <p><?php echo $first_name . " " . $last_name; ?></p>
-    </p>
-   
-    <p> 
-        <label for="doctor">Doctor:</label>
-        <select name="doctor" id="doctor">
-        <?php  while ($row = mysqli_fetch_array($doctorResult)): ?>
-      <option value=<? echo $row['id']; ?>><? echo $row['first_name'] . " " . $row['last_name']; ?></option>
-        <? endwhile ?>
-      </select>
-    </p>
+    <label for="roster_date">Roster Date:</label>
+    <input type="date" name="roster_date" value="<?php echo $roster_date; ?>" readonly >
+
+    <label for="doctor">Doctor:</label>
+
+    <select name="doctor" id="doctor">
+    <?php  while ($row = mysqli_fetch_array($doctorResult)): ?>
+    <option value=<? echo $row['id']; ?>><? echo $row['first_name'] . " " . $row['last_name']; ?></option>
+    <? endwhile ?>
+    </select>
     <input class="save" name="doctorAppointment" type="submit" value="Ok">
     <input class="cancel" type="reset" value="Cancel">
     <?php endif ?>
--- Accounts/doctors_appointment.php
+++ Accounts/doctors_appointment.php
@@ -7,53 +7,53 @@ if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-// if (isset($_POST['drAppointment_search'])) {
-//     $id = $_POST['id'];
-//     $sql= "SELECT a.*, r.* FROM accounts a, roles r WHERE a.role_id = r.role_id and r.role_name = 'patient' and id = $id";
+if (isset($_POST['drAppointment_search'])) {
+    $patient_id = $_POST['patient_id'];
+    $sql= "SELECT a.*, r.*, p.* FROM accounts a, roles r, patients p WHERE a.role_id = r.role_id and  r.role_name = 'patient' and p.patient_id = a.id and patient_id = $patient_id";
 
-//     $_SESSION['id'] = $id;
+    $_SESSION['patient_id'] = $patient_id;
 
-//     $result = mysqli_query($link, $sql);
-//     if (mysqli_num_rows($result) > 0) {
-//         while ($row = mysqli_fetch_array($result)){
-//         $first_name = $row['first_name'];
-//         $last_name = $row['last_name'];
-        
-//     }
-// }
-
-// else {
-//     $first_name = "error";
-//     $last_name = " incorrect ID";
-//     }
-// }
+    $result = mysqli_query($link, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)){
+            
+            $patient_id = $row['patient_id'];
+            $first_name = $row['first_name'];
+            $last_name = $row['last_name'];
+    }
+}
+else {
+    $first_name = "error";
+    $last_name = " incorrect ID";
+    }
+}
 if (isset($_POST['drAppointment_search'])) {
     $roster_date = $_POST['roster_date'];
     $_SESSION['roster_date'] = $roster_date;
-    $sql = "SELECT a.*, r.*, roster.* FROM accounts a, roles r , roster WHERE a.role_id = r.role_id and r.role_name = 'Doctor' and roster_date = $roster_date";
+    $sql = "SELECT a.*, roles.*, roster.* FROM accounts a, roles, roster WHERE a.role_id = roles.role_id and roles.role_id = 3 and a.id = roster.employee_id and roster.roster_date = '$roster_date';";
     $doctorResult = mysqli_query($link,$sql);
 }
 
 
+if (isset($_POST['doctorAppointment'])) {
+    $patient_id = $_POST["patient_id"];
+    $doctor = $_POST["doctor"];
+    $roster_date = $_POST["roster_date"];
+    $sql = "SELECT * FROM doctorAppointment WHERE patient_id = '$patient_id' and appointment_date = '$roster_date';";
+    $result = mysqli_query($link, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        echo "ERROR: the patient has been already appointed for that day";
+    }else {
+        $sql = "INSERT INTO doctorAppointment (patient_id, doctor_id, appointment_date) VALUES ('$patient_id', '$doctor', '$roster_date')";
+        if (mysqli_query($link, $sql)) {
+            echo "Records added successfully.";
+        } else {
 
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }
+    }
 
-// // Escape user inputs for security
-// if (isset($_POST['additional_Patient_info'])) {
-//     $group = $_POST["group"];
-//     $admission_date = $_POST["admission_date"];
-//     $p_id = $_SESSION['p_id'];
-// // Attempt insert query execution
-// $sql = "UPDATE accounts SET `patient_group` = '$group', `admission_date` = '$admission_date' WHERE id = $p_id";
-// $result = mysqli_query($link, $sql);
-// if (mysqli_query($link, $sql)) {
-//     echo "Records added successfully.";
-// } else {
-//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-// }
-
-// // Close connection
-// mysqli_close($link);
-
-// }
-
+}
+// Close connection
+mysqli_close($link);
 ?>
