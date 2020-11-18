4e59a0f9ba137bb7731472e39619a755127c2e9c
Charles Max Crandall <robloxatme.com@gmail.com>
2020-11-18 10:30:59 -0500

    Caregiver can now updated patient's dailycare table with correct true or false values for activities

--- views/caregiver.php
+++ views/caregiver.php
@@ -19,8 +19,6 @@ while ($row = mysqli_fetch_array($res)){
 }
 $sql = "SELECT c.id AS caregiverID, c.first_name AS caregiverFirstName, r.patient_group AS caregiverGroup, cr.role_id AS caregiverRole, p.id AS patientID, p.first_name AS patientFirstName, pa.patient_group AS patientGroup, pr.role_id AS patientRole, r.roster_date FROM accounts c, accounts p, roles cr, roles pr, roster r, patients pa WHERE p.role_id = pr.role_id and pr.access_level = 1 and c.role_id = cr.role_id and cr.access_level = 2 and c.id = r.employee_id and p.id = pa.patient_id and r.patient_group = pa.patient_group and r.patient_group = $patient_group and c.id = r.employee_id and r.roster_date = '$date'";
 
-//SELECT c.id AS caregiverID, c.first_name AS caregiverFirstName, r.patient_group AS caregiverGroup, cr.role_id AS caregiverRole, p.id AS patientID, p.first_name AS patientFirstName, pa.patient_group AS patientGroup, pr.role_id AS patientRole, r.roster_date FROM accounts c, accounts p, roles cr, roles pr, roster r, patients pa WHERE p.role_id = pr.role_id and pr.access_level = 1 and c.role_id = cr.role_id and cr.access_level = 2 and c.id = r.employee_id and p.id = pa.patient_id and r.patient_group = pa.patient_group and r.patient_group = 1 and c.id = r.employee_id and r.roster_date = '2020-11-17'
-
 $idArray = array();
 $nameArray = array();
 
@@ -40,13 +38,13 @@ if ($result = mysqli_query($link, $sql)) {
 
 
 if (isset($_POST['caregiver'])) {
-
   $list = $_POST['list'];
+
+  $checkID = 0;
 
   echo "You chose the following: <br>";
   if(!empty($list)){
   foreach ($list as $name){
-    echo $name . "<br />";
     $checkArray = explode('-', $name);
     $keys = array_keys($checkArray);
     foreach(array_keys($keys) as $value){
@@ -55,23 +53,48 @@ if (isset($_POST['caregiver'])) {
 
        $next_key = next($keys);
        $next_value = $checkArray[$next_key] ?? null;
-       echo  "{$value}: current = ({$current_key} => {$current_value}); next = ({$next_key} => {$next_value})\n";
 
        if (is_numeric($current_value)){
-         $first_name = $next_value;
+         $activity = $next_value;
+         if ($current_value != $checkID){
+           if ($checkID != 0){
+           $sql = "INSERT INTO dailyCare (patient_id, dailyCare_date, breakfast, lunch, dinner, morning_med, afternoon_med, night_med) VALUES ('$checkID', '$date', '$breakfast', '$lunch', '$dinner', '$morning_med', '$afternoon_med', '$night_med')";
+           $res = mysqli_query($link, $sql);
+          }
+           $breakfast = 0;
+           $lunch = 0;
+           $dinner = 0;
+           $morning_med = 0;
+           $afternoon_med = 0;
+           $night_med = 0;
+           $checkID = $current_value;
+         }
 
+         if ($activity == "breakfast"){
+           $breakfast = 1;
+         }
+         if ($activity == "lunch"){
+           $lunch = 1;
+         }
+         if ($activity == "dinner"){
+           $dinner = 1;
+         }
+         if ($activity == "morningMed"){
+           $morning_med = 1;
+         }
+         if ($activity == "afternoonMed"){
+           $afternoon_med = 1;
+         }
+         if ($activity == "nightMed"){
+           $night_med = 1;
+         }
     }
    }
   }
+  $sql = "INSERT INTO dailyCare (patient_id, dailyCare_date, breakfast, lunch, dinner, morning_med, afternoon_med, night_med) VALUES ('$checkID', '$date', '$breakfast', '$lunch', '$dinner', '$morning_med', '$afternoon_med', '$night_med')";
+  $res = mysqli_query($link, $sql);
  }
 }
-
-
-
-
-
-
-
 
 // Close connection
 mysqli_close($link);
